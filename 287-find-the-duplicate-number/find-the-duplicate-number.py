a3fd9ee4de9051f18a589b0fc1c7babed2c9f5b4
Arun Chaudhary <69865631+Arun6552@users.noreply.github.com>
2024-12-22 19:08:09 +0530

    Time: 151 ms (6.98%) | Memory: 30 MB (24.49%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,13 +1,24 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-       
 
-        hashnum = [0]* len(nums)
-        for i in nums:
-            if hashnum[i] != 1:
-                hashnum[i]=1
-            else:
-                return i 
+        low = 1
+        high = len(nums)-1
+        while (low < high) :
+            mid = (low + high)//2
+            count = 0 
+            for num in nums: 
+                if num <= mid:
+                    count +=1
+            if count > mid: 
+                high = mid
+            else: 
+                low = mid+1
+            
+        return low
+       
+ 
+
+        
 
 
         '''
@@ -18,6 +29,19 @@ class Solution:
                     return i 
                 else: 
                     ans.append(i) 
+
+        nums.sort()
+        for i in range(len(nums)+1):
+            if nums[i] == nums[i+1]:
+                return nums[i]
+
+               
+        hashnum = [0]* len(nums)
+        for i in nums:
+            if hashnum[i] != 1:
+                hashnum[i]=1
+            else:
+                return i 
         '''
         
       
