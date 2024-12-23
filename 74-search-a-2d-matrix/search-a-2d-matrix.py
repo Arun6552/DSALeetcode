b23d569da584a4523fcebfe8e19c0b29ddcbe3b1
Arun Chaudhary <69865631+Arun6552@users.noreply.github.com>
2024-12-23 11:54:40 +0530

    Time: 3 ms (5.32%) | Memory: 18.1 MB (5.93%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,13 +1,10 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        count = 0 
+        
         row = len(matrix)
         for i in range(row):
             if target in matrix[i]:
-                count +=1
-        if count >=1:
-            return True
-        else:
-            return False
+                return True
+        return False
 
            
