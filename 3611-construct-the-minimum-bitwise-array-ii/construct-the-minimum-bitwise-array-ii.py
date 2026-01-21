98d22158d0b48e153d5566fdf4839c3bcd952681
Arun Chaudhary <69865631+Arun6552@users.noreply.github.com>
2026-01-21 09:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (13.21%) - LeetSync

---
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minBitwiseArray(self, nums):
+        ans = []
+
+        for x in nums:
+            # Even numbers are impossible
+            if x % 2 == 0:
+                ans.append(-1)
+                continue
+
+            # Count trailing 1s
+            k = 0
+            temp = x
+            while temp & 1:
+                k += 1
+                temp >>= 1
+
+            # Minimum possible a
+            ans.append(x - (1 << (k - 1)))
+
+        return ans
